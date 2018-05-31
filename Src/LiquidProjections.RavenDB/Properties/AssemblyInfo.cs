6f5ad6ed72e00a9e56b1dc07ffbd94f2847d4caa
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2018-05-31 17:08:40 +0200

    The assemblies did not have the correct versions.

---
+++ Src/LiquidProjections.RavenDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LiquidProjections.RavenDB")]
+[assembly: AssemblyDescription("An extension to LiquidProjections that uses RavenDB for storing projections.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LiquidProjections.RavenDB")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright Dennis Doomen 2016-2018")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F5953539-5C27-4A7C-89BD-7FBE4A67915A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: AssemblyInformationalVersion("0.1.0-RelaxDependency.1+6.Branch.RelaxDependency.Sha.21d3f20c38d9aadb43d9dba95697e71c8178ed9d")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
